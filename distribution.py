08569eddb2048ea939e17b63b429f12d4132e808
esHathck05 <46973649+esHathck05@users.noreply.github.com>
2019-03-17 00:11:12 -0400

    Updated from Brython Server: 3/17/2019 12:11:09 AM

--- distribution.py
+++ distribution.py
@@ -1,59 +1,46 @@
 """
-random stuff that i used and did something cool:
-
-print(list(x*text.count(x) + str(len(list(x*text.count(x))))))
-
-mylist = (x*len(list(x*text.count(x))))
-        print(mylist)
-"""
-"""
 distribution.py
 Author: Esther Hacker
-Credit: N/A
-
-Assignment: Character Distribution
+Credit: n/a
+Assignment: character distribution
+Write and submit a Python program (distribution.py) that computes and displays 
+the distribution of characters in a given sample of text.
+Output of your program should look like this:
+Please enter a string of text (the bigger the better): The rain in Spain stays mainly in the plain.
+The distribution of characters in "The rain in Spain stays mainly in the plain." is:
+iiiiii
+nnnnnn
+aaaaa
+sss
+ttt
+ee
+hh
+ll
+pp
+yy
+m
+r
+Notice about this example:
+* The text: 'The rain ... plain' is provided by the user as input to your program.
+* Uppercase characters are converted to lowercase
+* Spaces and punctuation marks are ignored completely.
+* Characters that are more common appear first in the list.
+* Where the same number of characters occur, the lines are ordered alphabetically. 
+  For example, in the printout above, the letters e, h, l, p and y both occur twice 
+  in the text and they are listed in the output in alphabetical order.
+* Letters that do not occur in the text are not listed in the output at all.
 """
-
-text = input("Please enter a string of text (the bigger the better): ")
-print('The distribution of characters in "' + text + '" is: ')
-
 import string
-alphabet = string.ascii_lowercase
+stringboi = input("Please enter a string of text (the bigger the better): ")
+print('The distribution of characters in "' + stringboi + '" is: ')
+stringboi = stringboi.lower()
 
-for x in list(alphabet):
-    if x in str(text):
-        mylist = (x*text.count(x))
-        #print(mylist)
-        mylist2 = list(x + str(len(list(x*text.count(x)))))
-        print(mylist2)
-        #print(list(x*text.count(x) + str(len(list(x*text.count(x))))))
-        
-        
-        
-        
-def compare(a, b):
-    """
-    compare - generic comparison function for testing two elements.
-    """
-    return b > a
+emptystringboi = []
 
-
-def bsort(seq, cmp):
-    """
-    bsort - simple sorting algorithm that uses any comparison function
-    seq - a list to be sorted
-    cmp - a function for comparing two elements of seq
-    """
-    sorted = False  # assume the seq is not sorted to start with
-    while not sorted:
-        sorted = True   # assume it's already sorted correctly
-        for index, value in enumerate(seq): # for every element in seq
-            if index > 0:                   # past the first..
-                if not cmp(seq[index-1], value):  # if this element is out of order
-                    sorted = False          # then the list is not sorted yet
-                    seq[index-1], seq[index] = seq[index], seq[index-1] # and swap it
-
-    
-tosort = list(len(list(x*text.count(x))))
-bsort(tosort, compare)
-print(tosort)
+for x in list(string.ascii_lowercase):
+    if stringboi.count(x) != 0:
+        emptystringboi.append(x*stringboi.count(x))
+        
+evolvedstringboi = (sorted(emptystringboi, key=len, reverse = True))
+for y in evolvedstringboi:
+    print(y)
